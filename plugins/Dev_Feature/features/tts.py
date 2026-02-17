a84f9313926d2e8907cb2381b85fed01b2886c8a
˹  ᴘʀᴏᴠɪᴅᴇʀʙᴏᴛᴢ  ˼ <animeokids@gmail.com>
2026-02-17 21:23:30 +0530

    Add text-to-speech feature in tts.py
    Implement text-to-speech functionality using gTTS and Google Translate.

---
+++ plugins/Dev_Feature/features/tts.py
@@ -0,0 +1,41 @@
+# TEXT TO SPEECH PYTHON CODE
+import traceback
+from asyncio import get_running_loop
+from io import BytesIO
+from googletrans import Translator
+from gtts import gTTS
+from pyrogram import Client, filters
+from pyrogram.types import Message
+
+
+def convert(text):
+    audio = BytesIO()
+    i = Translator().translate(text, dest="en")
+    lang = i.src
+    tts = gTTS(text, lang=lang)
+    audio.name = lang + ".mp3"
+    tts.write_to_fp(audio)
+    return audio
+
+
+@Client.on_message(filters.command("tts"))
+async def text_to_speech(bot, message: Message):
+    safari = await bot.ask(chat_id = message.from_user.id, text = "Now send me your text.")
+    if safari.text:
+        m = await safari.reply_text("Processing")
+        text = safari.text
+        try:
+            loop = get_running_loop()
+            audio = await loop.run_in_executor(None, convert, text)
+            await safari.reply_audio(audio)
+            await m.delete()
+            audio.close()
+        except Exception as e:
+            await m.edit(e)
+            e = traceback.format_exc()
+            print(e)
+    else:
+        await safari.reply_text("Send me only text Buddy.")
+
+# CODE IS CREATED BY https://t.me/ProviderBotz PROVIDERBOTZ 
+# DO NOT REMOVE CREDIT....
